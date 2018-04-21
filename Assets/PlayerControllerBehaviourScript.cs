2439d0d1df4d471f3c55ce8b43f4a0617b5edfc2
Martin Hollstein <Hollsteinm@hotmail.com>
2018-04-21 10:20:15 -0500

    updated settins, added script file

---
+++ Assets/PlayerControllerBehaviourScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerBehaviourScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        var jumpPressed = Input.GetButtonDown("Jump");
+
+        var rightAxis = Input.GetAxis("RightMove");
+        var leanRightAxis = Input.GetAxis("RightLean");
+	}
+}
