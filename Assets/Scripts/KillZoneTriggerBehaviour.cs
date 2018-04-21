8258b70a1cfaed388c8cac619d8269ab2cec9dd9
Martin Hollstein <Hollsteinm@hotmail.com>
2018-04-21 16:45:50 -0500

    added kill plane

--- Assets/Scripts/KillZoneTriggerBehaviour.cs
+++ Assets/Scripts/KillZoneTriggerBehaviour.cs
@@ -1,16 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class KillZoneTriggerBehaviour : MonoBehaviour {
+	public string objectTagToKill = "Player";
+	public static string gameOverSceneName = "GameOver";
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.CompareTag(objectTagToKill)) {
+			SceneManager.LoadScene (gameOverSceneName);
+		}
 	}
 }
